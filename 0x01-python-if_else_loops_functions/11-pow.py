0a8f794f6508569849b2270833c731b1e9da739d
modusfas <modupefasunon@gmail.com>
2023-08-09 14:24:55 -0700

    updated

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author -Godswill Kalu
+# Author -Olumade modupe
 
 def pow(a, b):
     return (a ** b)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - Olumade Modupe
 
 def islower(c):
     """Function checks for lowercase characters."""
